fcf4b9b55b1f2e54a4240c4a7f90f52e50701065
Valeria Jara <valeria.jaraarenas@gmail.com>
2024-05-28 21:00:12 -0400

    Setup User Register Api with Encrypted Password

--- api/routes/User.js
+++ api/routes/User.js
@@ -3,26 +3,57 @@ const userRoute = express.Router();
 const AsyncHandler = require("express-async-handler");
 const User = require("../models/User");
 
-
 userRoute.post(
-    "/login",
-    AsyncHandler(async (req, res) => {
-      const { email, password } = req.body;
-      const user = await User.findOne({ email });
-      if (user  && (await user.matchPassword(password))) {
-        res.json({
-          _id: user.id,
+  "/login",
+  AsyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (user && (await user.matchPassword(password))) {
+      res.json({
+        _id: user.id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: null,
+        createdAt: user.createdAt,
+      });
+    } else {
+      res.status(401);
+      throw new Error("Invalid Email or Password");
+    }
+  })
+);
+
+//register route
+userRoute.post(
+  "/",
+  AsyncHandler(async (req, res) => {
+    const { name, email, password } = req.body;
+    const existUser = await User.findOne({ email });
+    if (existUser) {
+      res.status(400);
+      throw new Error("User Already exist");
+    } else {
+      const user = await User.create({
+        name,
+        email,
+        password,
+      });
+
+      if (user) {
+        res.status(201).json({
+          _id: user._id,
           name: user.name,
           email: user.email,
           isAdmin: user.isAdmin,
-          token: null,
           createdAt: user.createdAt,
         });
       } else {
-        res.status(401);
-        throw new Error("Invalid Email or Password");
+        res.status(400);
+        throw new Error("Invalid User Data");
       }
-    })
-  );
+    }
+  })
+);
 
-  module.exports = userRoute
+module.exports = userRoute;
