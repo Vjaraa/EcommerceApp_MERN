bf92cfa154e617a9dee30396cacf8d20e9e0bb4a
Valeria Jara <valeria.jaraarenas@gmail.com>
2024-06-17 18:55:58 -0400

    first commit

---
+++ my-project/src/components/Products.jsx
@@ -0,0 +1,145 @@
+const Products = () => {
+  return (
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="flex flex-wrap -m-4">
+          <div className="p-4 lg:w-1/4 md:w-1/2">
+            <div className="bg-white">
+              <div className="max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+                <div className="mt-6 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                  <div className="group relative">
+                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                      <img
+                        src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg"
+                        alt="Front of men&#039;s Polera básica in Negra."
+                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                      />
+                    </div>
+                    <div className="mt-4 flex justify-between">
+                      <div>
+                        <h3 className="text-sm text-gray-700">
+                          <a href="http://localhost:5173/ProductPoleraNegra">
+                            <span
+                              aria-hidden="true"
+                              className="absolute inset-0"
+                            ></span>
+                            Polera básica
+                          </a>
+                        </h3>
+                        <p className="mt-1 text-sm text-gray-500">Negra</p>
+                      </div>
+                      <p className="text-sm font-medium text-gray-900">$9.990</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-4 lg:w-1/4 md:w-1/2">
+            <div className="bg-white">
+              <div className="max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+                <div className="mt-6 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                  <div className="group relative">
+                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                      <img
+                        src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-02.jpg"
+                        alt="Front of men&#039;s Polera básica in Blanca."
+                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                      />
+                    </div>
+                    <div className="mt-4 flex justify-between">
+                      <div>
+                        <h3 className="text-sm text-gray-700">
+                          <a href="http://localhost:5173/ProductPoleraBlanca">
+                            <span
+                              aria-hidden="true"
+                              className="absolute inset-0"
+                            ></span>
+                            Polera básica
+                          </a>
+                        </h3>
+                        <p className="mt-1 text-sm text-gray-500">Blanca</p>
+                      </div>
+                      <p className="text-sm font-medium text-gray-900">$9.990</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-4 lg:w-1/4 md:w-1/2">
+            <div className="bg-white">
+              <div className="max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+                <div className="mt-6 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                  <div className="group relative">
+                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                      <img
+                        src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-03.jpg"
+                        alt="Front of men&#039;s Polera básica in Gris."
+                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                      />
+                    </div>
+                    <div className="mt-4 flex justify-between">
+                      <div>
+                        <h3 className="text-sm text-gray-700">
+                          <a href="http://localhost:5173/ProductPoleraGris">
+                            <span
+                              aria-hidden="true"
+                              className="absolute inset-0"
+                            ></span>
+                            Polera básica
+                          </a>
+                        </h3>
+                        <p className="mt-1 text-sm text-gray-500">Gris</p>
+                      </div>
+                      <p className="text-sm font-medium text-gray-900">$9.990</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-4 lg:w-1/4 md:w-1/2">
+            <div className="bg-white">
+              <div className="max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+                <div className="mt-6 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                  <div className="group relative">
+                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                      <img
+                        src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-04.jpg"
+                        alt="Front of men&#039;s Polera básica in Rosada."
+                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                      />
+                    </div>
+                    <div className="mt-4 flex justify-between">
+                      <div>
+                        <h3 className="text-sm text-gray-700">
+                          <a href="http://localhost:5173/ProductPoleraRosada">
+                            <span
+                              aria-hidden="true"
+                              className="absolute inset-0"
+                            ></span>
+                            Polera básica
+                          </a>
+                        </h3>
+                        <p className="mt-1 text-sm text-gray-500">Rosada</p>
+                      </div>
+                      <p className="text-sm font-medium text-gray-900">$10.990</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    
+    
+  );
+};
+
+export default Products;
